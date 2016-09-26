7d20f973866d525a86064d55ef937a381a20d6ae
Matthew Lemon <lemon@matthewlemon.com>
2016-09-26 13:21:24 +0100

    cleanup

--- master.py
+++ master.py
@@ -2,10 +2,9 @@ from bcmaster import BCMasterCSV
 import csv
 
 m = BCMasterCSV('source_files/master.csv', as_dataframe=True)
-
-m = m.flip()
+m.flip()
 
 with open('source_files/transposed.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row['SRO Full Name'])
+        print(row[''],"\t\t\t", row['SRO Full Name'])

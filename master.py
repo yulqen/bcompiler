f60981ee559befc427c6e81a688f77eac7c09e25
lemon <matt@matthewlemon.com>
2016-09-27 13:29:05 +0100

    lots of stuff missing from master.csv at this stage, but runs is source file untouched by Libreoffi ce

--- master.py
+++ master.py
@@ -12,29 +12,35 @@ dict_keys = m_dict.keys()
 
 dm = DataMap('source_files/datamap')
 dm.parse()
-#print(dm.output_excel_map_list)
 
 # let's open a blank template
 
 project1 = list_of_projects[10]
-wb = load_workbook('source_files/template.xlsx')
-ws = wb.get_sheet_by_name("Summary")
+wb = load_workbook('source_files/bicc_template.xlsx')
+ws_summary = wb.get_sheet_by_name("Summary")
+ws_fb = wb.get_sheet_by_name("Finance & Benefits")
 
 project1_data = m_dict[project1]
 project1_data['Project/Programme Name'] = project1
-# find relevant project
 
-print(ws['A5'].value)
-ws['B5'] = project1_data['Project/Programme Name']
-print("Successfully wrote data")
-wb.save('source_files/test1.xlsx')
-
-#for item in dm.output_excel_map_list:
-#    if item['sheet'] == 'Summary':
-#        try:
-#            summary_worksheet[item['cell_coordinates']].value = project1_data[item['cell_description']]
-#        except KeyError:
-#            print("Cannot find {} in master.csv".format(item['cell_description']))
-#            pass
+#print(ws['A5'].value)
+#ws['B5'] = project1_data['Project/Programme Name']
+#print("Successfully wrote data")
 #wb.save('source_files/test1.xlsx')
+
+for item in dm.output_excel_map_list:
+    if item['sheet'] == 'Summary':
+        try:
+            ws_summary[item['cell_coordinates']].value = project1_data[item['cell_description']]
+        except KeyError:
+            print("Cannot find {} in master.csv".format(item['cell_description']))
+            pass
+    elif item['sheet'] == 'Finance & Benefits':
+        try:
+            ws_fb[item['cell_coordinates']].value = project1_data[item['cell_description']]
+        except KeyError:
+            print("Cannot find {} in master.csv".format(item['cell_description']))
+            pass
+
+wb.save('source_files/test1.xlsx')
 

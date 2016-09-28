7f93d753fd950724c122c42bdcb1e93885a00030
Matthew Lemon <lemon@matthewlemon.com>
2016-09-28 13:40:21 +0100

    Finally got transpose working

--- bcompiler/bcompiler.py
+++ bcompiler/bcompiler.py
@@ -1,4 +1,5 @@
 import argparse
+import io
 import os
 
 
@@ -7,6 +8,8 @@ def get_parser():
     parser.add_argument('-c', '--clean-datamap', dest='datamap', nargs=1, help='clean datamap file'
                                                                                      'whose path is given as string')
     parser.add_argument('-v', '--version', help='displays the current version of bcompiler', action="store_true")
+    parser.add_argument('-p', '--parse', dest='parse', nargs=1, help='parse master.csv and flip'
+                                                                     ' to correct orientation')
     return parser
 
 
@@ -29,6 +32,25 @@ def _clean_datamap(source_file):
                 newline = newline + ',' + '\n'
                 cleaned_datamap.write(newline)
 
+
+def _parse_csv_to_file(source_file):
+    """
+    Transposes the master to a new master_transposed.csv file.
+    :param source_file:
+    :return:
+    """
+    output = open('source_files/master_transposed.csv', 'w+')
+    with open(source_file, 'r') as source_f:
+        lis = [x.split(',') for x in source_f]
+        for i in lis:
+            # we need to do this to remove trailing "\n" from the end of each original master.csv line
+            i[-1] = i[-1].rstrip()
+
+    for x in zip(*lis):
+        for y in x:
+            output.write(y + ',')
+        output.write('\n')
+
 def main():
     parser = get_parser()
     args = vars(parser.parse_args())
@@ -38,6 +60,10 @@ def main():
     if args['datamap']:
         _clean_datamap(args['datamap'][0])
         print("{} cleaned".format(args['datamap'][0]))
+        return
+    if args['parse']:
+        _parse_csv_to_file(args['parse'][0])
+        print("{} parsed and flipped to proper csv orientation and saved as new file".format(args['parse'][0]))
         return
 
 

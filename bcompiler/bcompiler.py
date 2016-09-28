da72fd03476b5498889a338d39651d304985c150
Matthew Lemon <lemon@matthewlemon.com>
2016-09-28 16:30:25 +0100

    battling on

--- bcompiler/bcompiler.py
+++ bcompiler/bcompiler.py
@@ -1,6 +1,9 @@
 import argparse
-import io
+import csv
 import os
+import re
+
+from openpyxl import load_workbook
 
 
 def get_parser():
@@ -10,8 +13,8 @@ def get_parser():
     parser.add_argument('-v', '--version', help='displays the current version of bcompiler', action="store_true")
     parser.add_argument('-p', '--parse', dest='parse', nargs=1, help='parse master.csv and flip'
                                                                      ' to correct orientation')
+    parser.add_argument('-b', '--populate-blanks', dest='populate', help='populate blank bicc forms from master')
     return parser
-
 
 def _clean_datamap(source_file):
 
@@ -51,6 +54,74 @@ def _parse_csv_to_file(source_file):
             output.write(y + ',')
         output.write('\n')
 
+
+def create_master_dict_transposed(source_master_csv):
+    _parse_csv_to_file(source_master_csv)
+    with open('source_files/master_transposed.csv', 'r') as f:
+        r = csv.DictReader(f)
+        l = [row for row in r]
+    return l
+
+
+def _get_list_projects(source_master_file):
+    reader = create_master_dict_transposed(source_master_file)
+    pl = [row['Project Name'] for row in reader]
+    return pl
+
+def get_datamap():
+    cell_regex = re.compile('[A-Z]+[0-9]+')
+    output_excel_map_list = []
+    f = open('source_files/cleaned_datamap', 'r')
+    data = f.readlines()
+    for line in data:
+        # split on , allowing us to access useful data from data map file
+        data_map_line = line.split(',')
+        if data_map_line[1] in ['Summary', 'Finance & Benefits', 'Resources', 'Approval and Project milestones',
+                                'Assurance planning']:
+            # the end item in the list is a newline - get rid of that
+            del data_map_line[-1]
+        if cell_regex.search(data_map_line[-1]):
+            try:
+                m_map = dict(cell_description=data_map_line[0],
+                             sheet=data_map_line[1],
+                             cell_coordinates=data_map_line[2])
+            except IndexError:
+                m_map = dict(cell_description=data_map_line[0],
+                             sheet="CAN'T FIND SHEET")
+            output_excel_map_list.append(m_map)
+    return output_excel_map_list
+
+def project_data_line(project):
+    dict = {}
+    with open('source_files/master_transposed.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if project in row['Project Name']:
+                dict = row
+    return dict
+
+
+def populate_blank_bicc_form(source_master_file):
+    datamap = get_datamap()
+    ls = _get_list_projects(source_master_file)
+    test_proj = ls[0]
+    blank = load_workbook('source_files/bicc_template.xlsx')
+    ws_summary = blank.get_sheet_by_name('Summary')
+    for item in datamap:
+        if item['sheet'] == 'Summary':
+            try:
+                test_proj_data = project_data_line(test_proj)
+                ws_summary[item['cell_coordinates']].value = test_proj_data[item['cell_description']]
+            except KeyError:
+                print("Cannot find {} in master.csv".format(item['cell_description']))
+                pass
+    blank.save('source_files/{}.xlsx'.format(test_proj))
+
+
+
+
+
+
 def main():
     parser = get_parser()
     args = vars(parser.parse_args())
@@ -63,9 +134,10 @@ def main():
         return
     if args['parse']:
         _parse_csv_to_file(args['parse'][0])
-        print("{} parsed and flipped to proper csv orientation and saved as new file".format(args['parse'][0]))
         return
-
+    if args['populate']:
+        populate_blank_bicc_form('source_files/master.csv')
+        return
 
 if __name__ == '__main__':
     main()

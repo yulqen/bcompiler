9f8fd0452c1a8103df7fca842317de486d5b699f
Matthew Lemon <lemon@matthewlemon.com>
2016-10-07 11:22:26 +0100

    some refactoring
    ridding us of private functions
    
    removed unnecessary params
    
    better start-up logic
    
    practicing setup

--- bcompiler/bcompiler.py
+++ bcompiler/bcompiler.py
@@ -1,18 +1,52 @@
+"""
+Copyright (c) 2016 Matthew Lemon
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
 import argparse
 import csv
 import os
 import re
 import shutil
 
+import sys
 from openpyxl import load_workbook
 from openpyxl.worksheet.datavalidation import DataValidation
 
 
+def working_directory(type=None):
+    """
+    Returns the working direct for source files
+    :return: path to the working directory intended for the source files
+    """
+    docs = os.path.join(os.path.expanduser('~'), 'Documents')
+    try:
+        bcomp_working_d = 'bcompiler'
+    except FileNotFoundError:
+        print("You need to run with --create-wd to create the working directory")
+    root_path = os.path.join(docs, bcomp_working_d)
+    if type == 'source':
+        return root_path + "/source/"
+    elif type == 'output':
+        return root_path + "/output/"
+    else:
+        return
+
+
+SOURCE_DIR = working_directory('source')
+OUTPUT_DIR = working_directory('output')
+DATAMAP = SOURCE_DIR + 'datamap'
+MASTER = SOURCE_DIR + 'master.csv'
+TEMPLATE = SOURCE_DIR + 'bicc_template.csv'
+
+
 def get_parser():
     parser = argparse.ArgumentParser(description='Compile BICC data or prepare Excel BICC return forms.')
-    parser.add_argument('-c', '--clean-datamap', dest='datamap', metavar='datamap file', nargs=1,
-                        help='clean datamap file'
-                             'whose path is given as string')
+    parser.add_argument('-c', '--clean-datamap', dest="clean-datamap", action="store_true", help='clean datamap file whose path is given as string')
     parser.add_argument('-v', '--version', help='displays the current version of bcompiler', action="store_true")
     parser.add_argument('-p', '--parse', dest='parse', metavar='source file', nargs=1, help='parse master.csv and flip'
                                                                                             ' to correct orientation')
@@ -27,14 +61,18 @@ def get_parser():
     return parser
 
 
-def _clean_datamap(source_file):
-    cleaned_datamap_file = 'source_files/cleaned_datamap'
+def clean_datamap():
+    dm = SOURCE_DIR + 'datamap'
+    cleaned_datamap_file = SOURCE_DIR + 'cleaned_datamap'
     try:
         os.remove(cleaned_datamap_file)
     except FileNotFoundError:
         pass
-    cleaned_datamap = open(cleaned_datamap_file, 'a+')
-    with open(source_file, 'r', encoding='UTF-8') as f:
+    try:
+        cleaned_datamap = open(cleaned_datamap_file, 'a+')
+    except FileNotFoundError:
+        create_working_directory()
+    with open(dm, 'r', encoding='UTF-8') as f:
         # make sure every line has a comma at the end
         for line in f.readlines():
             newline = line.rstrip()
@@ -46,13 +84,13 @@ def _clean_datamap(source_file):
                 cleaned_datamap.write(newline)
 
 
-def _parse_csv_to_file(source_file):
+def parse_csv_to_file(source_file):
     """
     Transposes the master to a new master_transposed.csv file.
     :param source_file:
     :return:
     """
-    output = open('source_files/master_transposed.csv', 'w+')
+    output = open(SOURCE_DIR + 'master_transposed.csv', 'w+')
     with open(source_file, 'r') as source_f:
         lis = [x.split(',') for x in source_f]
         for i in lis:
@@ -66,14 +104,14 @@ def _parse_csv_to_file(source_file):
 
 
 def create_master_dict_transposed(source_master_csv):
-    _parse_csv_to_file(source_master_csv)
-    with open('source_files/master_transposed.csv', 'r') as f:
+    parse_csv_to_file(source_master_csv)
+    with open(SOURCE_DIR + 'master_transposed.csv', 'r') as f:
         r = csv.DictReader(f)
         l = [row for row in r]
     return l
 
 
-def _get_list_projects(source_master_file):
+def get_list_projects(source_master_file):
     reader = create_master_dict_transposed(source_master_file)
     pl = [row['Project/Programme Name'] for row in reader]
     return pl
@@ -81,9 +119,9 @@ def _get_list_projects(source_master_file):
 
 def get_datamap():
     cell_regex = re.compile('[A-Z]+[0-9]+')
-    dropdown_headers = _get_dropdown_headers()
+    dropdown_headers = get_dropdown_headers()
     output_excel_map_list = []
-    f = open('source_files/cleaned_datamap', 'r')
+    f = open(SOURCE_DIR + 'cleaned_datamap', 'r')
     data = f.readlines()
     for line in data:
         # split on , allowing us to access useful data from data map file
@@ -119,7 +157,7 @@ def get_datamap():
 
 def project_data_line():
     p_dict = {}
-    with open('source_files/master_transposed.csv', 'r') as f:
+    with open(SOURCE_DIR + 'master_transposed.csv', 'r') as f:
         reader = csv.DictReader(f)
         for row in reader:
             key = row.pop('Project/Programme Name')
@@ -132,10 +170,10 @@ def project_data_line():
 def populate_blank_bicc_form(source_master_file, proj_num):
     datamap = get_datamap()
     proj_data = project_data_line()
-    ls = _get_list_projects(source_master_file)
+    ls = get_list_projects(source_master_file)
     test_proj = ls[int(proj_num)]
     test_proj_data = proj_data[test_proj]
-    blank = load_workbook('source_files/bicc_template.xlsx')
+    blank = load_workbook(SOURCE_DIR + 'bicc_template.xlsx')
     ws_summary = blank.get_sheet_by_name('Summary')
     ws_fb = blank.get_sheet_by_name('Finance & Benefits')
     ws_res = blank.get_sheet_by_name('Resources')
@@ -195,16 +233,31 @@ def populate_blank_bicc_form(source_master_file, proj_num):
                 ws_ap.add_data_validation(dv)
                 dv.add(ws_ap[item['cell_coordinates']])
 
-    blank.save('source_files/output/{}_Q2_Return.xlsx'.format(test_proj))
+    blank.save(OUTPUT_DIR + '{}_Q2_Return.xlsx'.format(test_proj))
 
 
 def pop_all():
-    number_of_projects = len(_get_list_projects('source_files/master.csv'))
+    number_of_projects = len(get_list_projects(SOURCE_DIR + 'master.csv'))
     for p in range(number_of_projects):
-        populate_blank_bicc_form('source_files/master.csv', p)
+        populate_blank_bicc_form(SOURCE_DIR + 'master.csv', p)
 
 
-def _create_working_directory():
+def check_for_correct_source_files():
+    docs = os.path.join(os.path.expanduser('~'), 'Documents')
+    bcomp_working_d = 'bcompiler'
+    if not os.path.exists(os.path.join(docs, bcomp_working_d)):
+        print("No working directory set up. Creating working directory.")
+        create_working_directory()
+        print("Please ensure the correct source files are installed:\n"
+              "\t\tsource/bicc_template.xlsx\n"
+              "\t\tsource/master.csv\n"
+              "\t\tsource/datamap\n")
+        sys.exit()
+    else:
+        return
+
+
+def create_working_directory():
     """
     We need a proper directory to work in.
     :return:
@@ -225,7 +278,7 @@ def _create_working_directory():
               "files persent.")
 
 
-def _delete_working_directory():
+def delete_working_directory():
     docs = os.path.join(os.path.expanduser('~'), 'Documents')
     bcomp_working_d = 'bcompiler'
     root_path = os.path.join(docs, bcomp_working_d)
@@ -236,13 +289,13 @@ def _delete_working_directory():
         return
 
 
-def _get_dropdown_data(header=None):
+def get_dropdown_data(header=None):
     """
     Pull the dropdown data from the Dropdown List sheet in bicc_template.xlsx. Location
     of this template file might need to be dynamic.
     :return tuple of column values from sheet, with header value at list[0]:
     """
-    wb = load_workbook('source_files/bicc_template.xlsx', data_only=True)
+    wb = load_workbook(SOURCE_DIR + 'bicc_template.xlsx', data_only=True)
     ws = wb.get_sheet_by_name('Dropdown List')
     columns = ws.columns
     col_lis = [col for col in columns]
@@ -256,8 +309,8 @@ def _get_dropdown_data(header=None):
         return dropdown_data
 
 
-def _get_dropdown_headers():
-    wb = load_workbook('source_files/bicc_template.xlsx', data_only=True)
+def get_dropdown_headers():
+    wb = load_workbook(SOURCE_DIR + 'bicc_template.xlsx', data_only=True)
     ws = wb.get_sheet_by_name('Dropdown List')
     rows = ws.rows
     a_row = next(rows)
@@ -335,33 +388,35 @@ def create_validation(header):
 def main():
     parser = get_parser()
     args = vars(parser.parse_args())
+    check_for_correct_source_files()
     if args['version']:
         print("1.0")
         return
-    if args['datamap']:
-        _clean_datamap(args['datamap'][0])
-        print("{} cleaned".format(args['datamap'][0]))
+    if args['clean-datamap']:
+        clean_datamap()
+        print("datamap cleaned")
         return
     if args['parse']:
-        _parse_csv_to_file(args['parse'][0])
+        parse_csv_to_file(args['parse'][0])
         return
     if args['populate']:
-        _clean_datamap('source_files/datamap')
-        _parse_csv_to_file('source_files/master.csv')
-        populate_blank_bicc_form('source_files/master.csv', args['populate'])
+        master = '{}master.csv'.format(working_directory('source'))
+        clean_datamap()
+        parse_csv_to_file(master)
+        populate_blank_bicc_form(master, args['populate'])
         return
     if args['all']:
         pop_all()
         return
     if args['create-wd']:
-        _create_working_directory()
+        create_working_directory()
         return
     if args['f-create-wd']:
         print("This will destroy your existing working directory prior to creating a new one.\n\nAre you sure?")
         response = input('(y/n) --> ')
         if response in ('y', 'ye', 'yes', 'Y', 'YES'):
-            _delete_working_directory()
-            _create_working_directory()
+            delete_working_directory()
+            create_working_directory()
             return
         else:
             return
---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import bcompiler
+import os
+
+def read(*names):
+    values = dict()
+    extensions = ['.txt', '.rst']
+    for name in names:
+        value = ''
+        for extension in extensions:
+            filename = name + extension
+            if os.path.isfile(filename):
+                value = open(name + extension).read()
+                break
+        values[name] = value
+    return values
+
+long_description = """
+%(README)s
+
+News
+====
+
+%(CHANGES)s
+
+""" % read('README', 'CHANGES')
+
+setup(
+    name='bcompiler',
+    version=bcompiler.__version__,
+    description='Program to migrate data for BICC reporting process.',
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Documentation",
+    ],
+    keywords='bcomiler help console command line excel',
+    author='Matthew Lemon',
+    author_email='matt@matthewlemon.com',
+    maintainer='Matthew Lemon',
+    maintainer_email='matt@matthewlemon.com',
+    url='https://bitbucket.org/mrlemon/bicc_excel',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'bcompiler = bcomiler.bcompiler:main',
+        ]
+    },
+    install_requires=[
+                         'openpyxl',
+                     ]
+)

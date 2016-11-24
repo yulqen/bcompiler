928dec4f47f05c6a646e205406ebad344796b3db
Matthew Lemon <lemon@matthewlemon.com>
2016-11-24 12:16:07 +0000

    more detail on the test

--- bcompiler/tests/test_master_clean_xlsx.py
+++ bcompiler/tests/test_master_clean_xlsx.py
@@ -1,3 +1,12 @@
+"""
+After data from the BICC forms is migrated into a compiled_master.xlsx file,
+we need to avoid having to re-save this file as csv and then manually removing
+all in-cell commas, so that we can create a transposed_master elsewhere in the
+application, because that's a major pain in the backside. So what we're going
+here is performing clean-up functions on the resulting compiled_master.xlsx
+file that is created straight after the BICC forms compilation. That will then
+absolve us from all hassle.
+"""
 import pytest
 
 from openpyxl import Workbook
@@ -10,6 +19,7 @@ def dirty_master():
     ws = wb.active
     top_row = ws['A1':'A5']
     second_row = ws['B1':'B5']
+    third_row = ws['C1':'C5']
     for cell in top_row:
         cell[0].value = "Header {}".format(inc)
         inc += 1
@@ -17,6 +27,9 @@ def dirty_master():
     for cell in second_row:
         cell[0].value = "Data {}".format(inc)
         inc += 1
+    # now we're going to put some horrible commas in these cells
+    for cell in third_row:
+        cell[0].value = "Garbage data, with commas!"
     return wb
 
 
@@ -29,3 +42,6 @@ def test_phantom_wb(dirty_master):
     assert ws['A1'].value == 'Header 0'
     assert ws['A2'].value == 'Header 1'
     assert ws['B1'].value == 'Data 0'
+    assert ',' in ws['C1'].value
+    assert 'X' not in ws['C1'].value
+    assert 'Garbage' in ws['C1'].value

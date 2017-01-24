911b61efad1c0db157db5cb486cdde5bce2e784c
Matthew Lemon <lemon@matthewlemon.com>
2017-01-24 11:08:18 +0000

    --wip-- [skip ci]

--- bcompiler/tests/fixtures.py
+++ bcompiler/tests/fixtures.py
@@ -2,7 +2,7 @@ import os
 import pytest
 
 from bcompiler.process.database import Database
-from bcompiler.process.digest import digest_source_files, Digest, Series
+from bcompiler.process.digest import Series
 
 from openpyxl import Workbook
 
@@ -11,15 +11,33 @@ from openpyxl import Workbook
 def bicc_return():
     wb = Workbook()
     wb.create_sheet('Summary')
-    wb.create_sheet('Approval & Project milestones')
     wb.create_sheet('Finance & Benefits')
+    wb.create_sheet('Approval & Project milestones')
     wb.create_sheet('Resources')
     wb.create_sheet('Assurance planning')
     wb.create_sheet('GMPP info')
-    ws = wb['Summary']
+    ws_summary = wb['Summary']
     # enter some values in the right slots
-    ws['B5'].value = 'Cookfield Rebuild'
-    ws['B8'].value = 'Roads, Monitoring and Horse'
+    ws_summary['A5'].value = 'Project/Programme Name'
+    ws_summary['B5'].value = 'Cookfield Rebuild'
+    ws_summary['A8'].value = 'DfT Group'
+    ws_summary['B8'].value = 'Roads, Monitoring and Horse'
+
+    ws_finance = wb['Finance & Benefits']
+    ws_finance['A6'].value = 'SRO Finance Confidence'
+    ws_finance['C6'].value = 'Red'
+    ws_finance['B11'].value = 'Date of Business Case'
+    ws_finance['A19'].value = 'Index Year'
+    ws_finance['B19'].value = '2012'
+    ws_finance['A18'].value = 'Real or Nominal'
+    ws_finance['C18'].value = 'Nominal'
+    ws_finance['A36'].value = '2019/2020'
+    ws_finance['C36'].value = 2.00
+    ws_finance['A44'].value = 'Total'
+    ws_finance['C44'].value = 23.30033
+    ws_finance['A77'].value = 'Total WLC (RDEL)'
+    ws_finance['C77'].value = 232.32
+
 
     wb.save('/tmp/test-bicc-return.xlsx')
     yield '/tmp/test-bicc-return.xlsx'

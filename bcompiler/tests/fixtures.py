9f8a4826a4248e49939e4d77d050c26e1f8bc69f
Matthew Lemon <lemon@matthewlemon.com>
2017-01-24 11:21:49 +0000

    --wip-- [skip ci]

--- bcompiler/tests/fixtures.py
+++ bcompiler/tests/fixtures.py
@@ -16,13 +16,15 @@ def bicc_return():
     wb.create_sheet('Resources')
     wb.create_sheet('Assurance planning')
     wb.create_sheet('GMPP info')
+
+    # Summary fixture
     ws_summary = wb['Summary']
-    # enter some values in the right slots
     ws_summary['A5'].value = 'Project/Programme Name'
     ws_summary['B5'].value = 'Cookfield Rebuild'
     ws_summary['A8'].value = 'DfT Group'
     ws_summary['B8'].value = 'Roads, Monitoring and Horse'
 
+    # Finance & Benefits fixture
     ws_finance = wb['Finance & Benefits']
     ws_finance['A6'].value = 'SRO Finance Confidence'
     ws_finance['C6'].value = 'Red'
@@ -38,6 +40,24 @@ def bicc_return():
     ws_finance['A77'].value = 'Total WLC (RDEL)'
     ws_finance['C77'].value = 232.32
 
+    # Resources fixture
+    ws_resources = wb['Resources']
+    ws_resources['A7'].value = 'SCS(PB2)'
+    ws_resources['C7'].value = 1.00
+    ws_resources['A17'].value = 'Total'
+    ws_resources['G17'].value = 0.0
+    ws_resources['A30'].value = 'Change Implementation'
+    ws_resources['I30'].value = 'Green'
+    ws_resources['J30'].value = 'Amber'
+    ws_resources['G38'].value = 'Overall Assessment'
+    ws_resources['J38'].value = 'Red'
+
+    # Approval and Project Milestones fixture
+    ws_approvals = wb['Approval & Project milestones']
+    ws_approvals['A10'].value = 'SOBC - HMT Approval'
+    ws_approvals['C10'].value = '20/2/2009'
+    ws_approvals['A19'].value = 'FBC - HMT Approval'
+    ws_approvals['F19'].value = 'A lot of very uninteresting test text here.'
 
     wb.save('/tmp/test-bicc-return.xlsx')
     yield '/tmp/test-bicc-return.xlsx'

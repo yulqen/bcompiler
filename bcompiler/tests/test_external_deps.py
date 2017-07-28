246e2044da5642e4fc2e302e570a9e3cc321ca0b
Matthew Lemon <lemon@matthewlemon.com>
2017-07-28 11:44:27 +0100

    moved test values into config file

--- bcompiler/tests/test_external_deps.py
+++ bcompiler/tests/test_external_deps.py
@@ -5,10 +5,14 @@ including the datamap and the bicc_template.xlsx.
 If the template changes, the cell references in test_generated_template()
 function below MUST be amended accordingly.
 """
+import configparser
 import csv
 
 from openpyxl import load_workbook
 
+config = configparser.ConfigParser()
+CONFIG_FILE = 'test_config.ini'
+config.read(CONFIG_FILE)
 
 def test_existence(datamap):
     with open(datamap, 'r', newline='') as f:
@@ -19,20 +23,20 @@ def test_existence(datamap):
 
 def test_generated_template(blank_template):
     wb = load_workbook(blank_template)
-    sheet_s = wb['Summary']
-    sheet_fb = wb['Finance & Benefits']
-    sheet_r = wb['Resource']
-    sheet_apm = wb['Approval & Project milestones']
-    sheet_ap = wb['Assurance Planning']
-    assert sheet_s['A8'].value == 'DfT Group'
-    assert sheet_s['A46'].value == 'SRO Overall Delivery Confidence'
-    assert sheet_fb['A121'].value == '2027/2028'
-    assert sheet_fb['F26'].value == 'All RDEL (WLC) Total'
-    assert sheet_r['A36'].value == 'Other (please specify)'
-    assert sheet_r['A12'].value == 'SEO (PB5)'
-    assert sheet_apm['E7'].value == 'Type of milestone'
-    assert sheet_ap['B32'].value == 'External'
-    assert sheet_ap['C31'].value == None
+    sheet_s = wb[config['TemplateTestData']['summary_sheet']]
+    sheet_fb = wb[config['TemplateTestData']['fb_sheet']]
+    sheet_r = wb[config['TemplateTestData']['resource']]
+    sheet_apm = wb[config['TemplateTestData']['apm']]
+    sheet_ap = wb[config['TemplateTestData']['ap']]
+    assert sheet_s['A8'].value == config['SummaryData']['A8']
+    assert sheet_s['A46'].value == config['SummaryData']['A46']
+    assert sheet_fb['A121'].value == config['FinanceData']['A121']
+    assert sheet_fb['F26'].value == config['FinanceData']['F26']
+    assert sheet_r['A36'].value == config['ResourceData']['A36']
+    assert sheet_r['A12'].value == config['ResourceData']['A12']
+    assert sheet_apm['E7'].value == config['ApprovalProjectMilestones']['E7']
+    assert sheet_ap['B32'].value == config['AssurancePlanning']['B32']
+    assert sheet_ap['C31'].value == None  # can't put None value in config file
 
 def test_incorrect_template_cells(blank_template):
     wb = load_workbook(blank_template)

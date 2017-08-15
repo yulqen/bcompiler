ee638c793b2248da29ec50ef8b67d8006fa4662f
Matthew Lemon <lemon@matthewlemon.com>
2017-08-15 16:03:41 +0100

    added some docstrings to tests so I know what's happening

--- bcompiler/tests/test_file_parsing.py
+++ bcompiler/tests/test_file_parsing.py
@@ -9,12 +9,18 @@ from openpyxl import load_workbook
 
 
 def test_compile(populated_template, datamap):
+    """
+    This tests bcompiler -b X essentially (or bcompiler -a, but for a single file.
+    """
     data = parse(populated_template, datamap)
     assert data[0]['gmpp_key'] == 'Project/Programme Name'
     assert data[0]['gmpp_key_value'] == 'PROJECT/PROGRAMME NAME 9'
 
 
 def test_run(datamap):
+    """
+    This tests 'bcompiler compile' or 'bcompiler' option.
+    """
     # print([item for item in dir(compile_module) if not item.startswith("__")])
     # patching module attributes to get it working
     setattr(compile_module, 'RETURNS_DIR', '/tmp/bcompiler-test/')
@@ -25,6 +31,9 @@ def test_run(datamap):
 
 
 def test_datamap_class(datamap):
+    """
+    This tests correct creation of Datamap object.
+    """
     dm = Datamap()
     dm.cell_map_from_csv(datamap)
     assert dm.cell_map[1].cell_reference == 'B49'

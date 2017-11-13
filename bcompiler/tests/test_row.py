99e283fa3d12c4cf719590840293732b1da83015
Matthew Lemon <lemon@matthewlemon.com>
2017-11-13 21:35:00 +0000

    slight amendment

--- bcompiler/tests/test_row.py
+++ bcompiler/tests/test_row.py
@@ -19,6 +19,21 @@ def test_for_basic_row_object_given_list(tmpdir):
     assert ws['D1'].value is None
 
 
+def test_for_basic_row_object_given_tuple(tmpdir):
+    values_l = ('Test Value A1', 'Test Value B1', 'Test Value C1')
+    wb = Workbook()
+    ws = wb.active
+    r = Row(1, 1, values_l)
+    r.bind(ws)
+    wb.save(tmpdir.join('test_row_object.xlsx'))
+    loaded_wb = load_workbook(tmpdir.join('test_row_object.xlsx'))
+    ws = loaded_wb.active
+    assert ws['A1'].value == 'Test Value A1'
+    assert ws['B1'].value == 'Test Value B1'
+    assert ws['C1'].value == 'Test Value C1'
+    assert ws['D1'].value is None
+
+
 def test_row_object_given_column_reference_as_string(tmpdir):
     values_l = ['Test Value A1', 'Test Value B1', 'Test Value C1']
     wb = Workbook()
@@ -51,12 +66,10 @@ def test_row_object_given_column_reference_as_double_string(tmpdir):
 
 def test_bullshit_cell_reference(tmpdir):
     values_l = ['Test Value AA1', 'Test Value AB1', 'Test Value AC1']
-    wb = Workbook()
-    ws = wb.active
     with pytest.raises(ValueError) as excinfo:
-        r = Row('AAA', 1, values_l)
+        Row('AAA', 1, values_l)
     assert "You can only have a column up to AZ" in str(excinfo.value)
     with pytest.raises(ValueError) as excinfo:
-        r = Row('AADDB', 1, values_l)
+        Row('AADDB', 1, values_l)
     assert "You can only have a column up to AZ" in str(excinfo.value)
 

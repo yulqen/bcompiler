6188572e4e395a415839993b2cf4ec3154746b6a
Matthew Lemon <lemon@matthewlemon.com>
2017-11-17 12:26:50 +0000

    passing tests

--- bcompiler/core/master.py
+++ bcompiler/core/master.py
@@ -1,5 +1,6 @@
 from ..utils import project_data_from_master
 from pathlib import Path
+from typing import List, Tuple
 
 
 class ProjectData:
@@ -10,13 +11,28 @@ class ProjectData:
         """
         ordered_dict is easiest to get from project_data_from_master[x]
         """
-        self.data = d
+        self._data = d
 
-    def __len__(self):
-        return len(self.data)
+    def __len__(self) -> int:
+        return len(self._data)
 
     def __getitem__(self, item):
-        return self.data[item]
+        return self._data[item]
+
+    def key_filter(self, key: str) -> List[Tuple]:
+        data = [item for item in self._data.items() if key in item[0]]
+        if not data:
+            raise KeyError("Sorry, there is no matching data")
+        return (data)
+
+    def pull_keys(self, input_iter) -> List:
+        """
+        Returns a list of (key, value) tuples from ProjectData if key matches a
+        key. The order of tuples is based on the order of keys passed in the iterable.
+        """
+        xs = [item for item in self._data.items() for i in input_iter if item[0] == i]
+        ts = sorted(xs, key=lambda x: input_iter.index(x[0]))
+        return ts
 
 
 class Master:
--- bcompiler/tests/test_master.py
+++ bcompiler/tests/test_master.py
@@ -1,3 +1,5 @@
+import pytest
+
 from ..core import Master, Quarter, ProjectData
 
 
@@ -30,3 +32,40 @@ def test_project_data_object(master):
     assert m.year == 2018
     assert len(m.projects) == 3
     assert isinstance(m['PROJECT/PROGRAMME NAME 1'], ProjectData)
+
+
+def test_master_key_filter(master):
+    q1_2017 = Quarter(1, 2017)
+    m1 = Master(q1_2017, master)
+    for t in m1['PROJECT/PROGRAMME NAME 1'].key_filter('SRO'):
+        try:
+            assert 'SRO FULL NAME 1' in t[1]
+        except (AssertionError, TypeError):
+            continue
+
+
+def test_master_key_filter_missing_key(master):
+    q1_2017 = Quarter(1, 2017)
+    m1 = Master(q1_2017, master)
+    with pytest.raises(KeyError):
+        for t in m1['PROJECT/PROGRAMME NAME 1'].key_filter('NOT HERE'):
+            continue
+
+
+def test_pull_iterable_from_master_based_on_key(master):
+    p1 = Master(Quarter(1, 2017), master)['PROJECT/PROGRAMME NAME 1']
+#   assert p1.pull_keys(['SRO Full Name']) == [('SRO Full Name', 'SRO FULL NAME 1')]
+#   assert p1.pull_keys(['Quarter Joined']) == [('Quarter Joined', 'QUARTER JOINED 1')]
+    assert p1.pull_keys(['SRO Full Name', 'Quarter Joined']) == [
+        ('SRO Full Name', 'SRO FULL NAME 1'),
+        ('Quarter Joined', 'QUARTER JOINED 1'),
+    ]
+
+
+
+#def test_multiple_masters(master, previous_quarter_master):
+#    q2_2017 = Quarter(2, 2017)
+#    q1_2017 = Quarter(1, 2017)
+#    m2 = Master(q1_2017, master)
+#    m1 = Master(q1_2017, previous_quarter_master)
+

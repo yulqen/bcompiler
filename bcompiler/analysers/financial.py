6a6ebcfbda44d423881c8e2d3dc9d8893fe34ddc
Matthew Lemon <lemon@matthewlemon.com>
2017-11-17 16:22:20 +0000

    on the way to implementing the analyser for finances, but got a fair way to go

--- bcompiler/analysers/financial.py
+++ bcompiler/analysers/financial.py
@@ -5,24 +5,23 @@ from openpyxl import load_workbook, Workbook
 
 def run(masters_list):
     wb = Workbook()
-    ws = wb.active
     q1 = Quarter(1, 2017)
     q2 = Quarter(2, 2017)
     start_row = 1
     target_keys = ['RDEL Total Forecast', 'CDEL Total Forecast']
     for m in masters_list:
-        mo = Master(q1, m)
-        projects = mo.projects
+        master = Master(q1, m)
+        projects = master.projects
         for p in projects:
-            p_data = mo[p]
+            ws = wb.create_sheet(p)
+            p_data = master[p]
             d = p_data.pull_keys(target_keys, flat=True)
             header = Row(2, start_row + 1, target_keys)
             r = Row(2, start_row + 2, d)
-            ws.cell(row=start_row + 2, column=1, value=str(mo.quarter))
+            ws.cell(row=start_row + 2, column=1, value=str(master.quarter))
             header.bind(ws)
             r.bind(ws)
             ws.cell(row=start_row, column=1, value=p)
-            start_row += 3
     wb.save('/tmp/baws.xlsx')
 
 

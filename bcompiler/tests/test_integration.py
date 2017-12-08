3fe987a7f59deb9be34445aba8c7c87a3455c6eb
Matthew Lemon <lemon@matthewlemon.com>
2017-12-08 08:30:52 +0000

    three integration tests now present

--- bcompiler/tests/test_integration.py
+++ bcompiler/tests/test_integration.py
@@ -1,6 +1,18 @@
 import subprocess
 
+from bcompiler import __version__
+
 
 def test_bcompiler_help():
     output = subprocess.run(['bcompiler', '-h'], stdout=subprocess.PIPE, encoding='utf-8')
     assert output.stdout.startswith('usage')
+
+
+def test_bcompiler_version():
+    output = subprocess.run(['bcompiler', '-v'], stdout=subprocess.PIPE, encoding='utf-8')
+    assert output.stdout.strip() == __version__
+
+
+def test_bcompiler_count_rows(populated_template):
+    output = subprocess.run(['bcompiler', '-r'], stdout=subprocess.PIPE, encoding='utf-8')
+    assert output.stdout.startswith('Workbook')

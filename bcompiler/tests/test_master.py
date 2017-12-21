6f78fefa191b799c1e476692bccbafe120757a66
Matthew Lemon <lemon@matthewlemon.com>
2017-12-21 11:37:23 +0000

    renamed ProjectData class to MasterProjectData

--- bcompiler/tests/test_master.py
+++ bcompiler/tests/test_master.py
@@ -1,7 +1,7 @@
 import pytest
 import logging
 
-from ..core import Master, Quarter, ProjectData
+from ..core import Master, Quarter, MasterProjectData
 
 
 def test_master(master):
@@ -32,7 +32,7 @@ def test_project_data_object(master):
     assert str(m.quarter) == 'Q2 18/19'
     assert m.year == 2018
     assert len(m.projects) == 3
-    assert isinstance(m['PROJECT/PROGRAMME NAME 1'], ProjectData)
+    assert isinstance(m['PROJECT/PROGRAMME NAME 1'], MasterProjectData)
 
 
 def test_master_key_filter(master):
